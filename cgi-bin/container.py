e217c331602aa0ef983b146844a0adf73ae05334
Patrick Drummond <erissian@gmail.com>
2020-02-18 15:42:52 -0500

    POST data expectations listed

--- cgi-bin/container.py
+++ cgi-bin/container.py
@@ -1,3 +1,26 @@
+# This script takes POST data
+
+# POST variables expected:
+
+# cwidth    - Container width
+# cdepth    - Container depth
+# cheight   - Container height
+
+# name[]    - Array of package identifiers
+# length[]  = Array of package depths
+# width[]   = Array of package widths
+# height[]  - Array of package heights
+# qty[]     - Array of how many individual packages this entry represents
+
+# ***** WARNING ABOUT FLAGS *****
+# Flags are usually handled by checkboxes. However, POST data only sends the checkboxes that have been checked,
+# with no indication which row it belongs to. Therefore, every checkbox should be overwritten to send a value
+# of either 'checked' or 'unchecked'
+
+# top[]     = Array of flags for whether this package may have others below it
+# bottom[]  - Array of flags for whether this package may have others below it
+# rotation[]- Array of flags for whether this package may be rotated in space or not    
+
 logpath = './log'
 
 import sys
